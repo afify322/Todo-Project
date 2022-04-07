3cb7852deee4bbe4f56fb43074c926c140fa5883
mohamed ashrf 3afify <afify322@gmail.com>
2022-04-07 02:29:21 +0200

    "fixed"

--- helpers/session.js
+++ helpers/session.js
@@ -4,8 +4,8 @@ const MongoDBStore=require('connect-mongodb-session')(session)
 const url='mongodb://localhost:27017';
 
 const store=new MongoDBStore({
-  uri:url,
-  // uri:'mongodb+srv://first:6PhsjC3EuCp4z9oy@cluster0.kb4eg.mongodb.net/clinc?authSource=admin&replicaSet=atlas-spouhm-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+ // uri:url,
+   uri:'mongodb+srv://first:6PhsjC3EuCp4z9oy@cluster0.kb4eg.mongodb.net/clinc?authSource=admin&replicaSet=atlas-spouhm-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
    collection:"session",
     databaseName:"todos"
   })
